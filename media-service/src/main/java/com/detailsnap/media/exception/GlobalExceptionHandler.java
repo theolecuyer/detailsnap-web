924891e9359f2a8e87142401fc6ff3a42c1ef6ed
Theo Lecuyer <theoglecuyer@gmail.com>
2026-05-09 00:11:15 -0700

    debug: log unhandled exceptions in media-service

--- media-service/src/main/java/com/detailsnap/media/exception/GlobalExceptionHandler.java
+++ media-service/src/main/java/com/detailsnap/media/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.detailsnap.media.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +13,8 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<?> handleMaxSize(MaxUploadSizeExceededException e) {
         return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
@@ -19,6 +23,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGeneral(Exception e) {
+        log.error("Unhandled exception", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Map.of("error", "Internal server error", "code", "INTERNAL_ERROR"));
     }
